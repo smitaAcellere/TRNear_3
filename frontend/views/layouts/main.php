876dc4e16e104702c965cbaa606fa319a0fca72b
Aditya Ogale <aditya.ogale@acellere.com>
2016-10-13 12:56:11 +0530

    Successful Subscription message added

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -91,7 +91,6 @@ AppAsset::register($this);
                         'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
                     ])
                     ?>
-                    <?= Alert::widget() ?>
                     <?= $content ?>
                 </div>
             </main>
--- frontend/views/site/newsletter.php
+++ frontend/views/site/newsletter.php
@@ -16,23 +16,31 @@ $this->title = 'Subscribe';
         <h1>Large Headline Here</h1>
         <hr>
         <p>Vestibulum rutrum quam vitae fringilla tincidunt. Suspendisse nec tortor urna. Ut laoreet sodales nisi, quis iaculis nulla iaculis vitae. Donec sagittis faucibus lacus eget blandit..</p>
+        <?php if (Yii::$app->session->hasFlash('success')) { ?>
+            <div class="alert-success alert fade in" id="w1-success-0">
+                <button aria-hidden="true" data-dismiss="alert" class="close" type="button">×</button>
+
+                <?= Yii::$app->session->getFlash('success') ?>
+
+            </div>
+
+        <?php } ?>
+
         <?php $form = ActiveForm::begin(); ?>
 
-
-
         <div class="form-group">
-                <?=
-                $form->field($model, 'email_id', [
-                    'addon' => [
-                        'append' => [
-                            'content' => Html::submitButton('Notify Me!', ['id'=>'submit-email-button','class' => 'btn btn-lg btn-primary', 'name' => 'signup-button']),
-                            'asButton' => true
-                        ]
+            <?=
+            $form->field($model, 'email_id', [
+                'addon' => [
+                    'append' => [
+                        'content' => Html::submitButton('Notify Me!', ['id' => 'submit-email-button', 'class' => 'btn btn-lg btn-primary', 'name' => 'signup-button']),
+                        'asButton' => true
                     ]
-                ])->textInput(['autofocus' => true, 'placeholder' => 'Email Address'])->label(false)
-                ?>
+                ]
+            ])->textInput(['autofocus' => true, 'placeholder' => 'Email Address'])->label(false)
+            ?>
         </div>
 
-<?php ActiveForm::end(); ?>
+        <?php ActiveForm::end(); ?>
     </div>
 </div>
